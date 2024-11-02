31202f8b45a35dbdc9ae082e425cac08e5a0bc69
thorn132 <152637753+thorn132@users.noreply.github.com>
2024-11-02 15:22:07 -0600

    App stubs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{application, Element, Task};
+
+fn main() -> iced::Result {
+    application("TabDoc", App::update, App::view).run_with(App::new)
+}
+
+struct App;
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl App {
+    fn new() -> (Self, Task<Message>) {
+        (App, Task::none())
+    }
+
+    fn update(&mut self, _message: Message) -> Task<Message> {
+        Task::none()
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        "".into()
+    }
 }
